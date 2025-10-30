211747b29ab7bfdc002a5d84b5d42bbc5b15a29e
jennilaluyan <laluyanjennifer11@gmail.com>
2025-10-31 05:17:55 +0800

    chore: repo hygiene + lint/format setup (ESLint/Prettier/Pint) and line-ending normalization

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2023: true, node: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "detect" } },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+  }
+};
